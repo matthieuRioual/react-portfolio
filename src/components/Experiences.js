32de5ce6945082472813072e1e4119fad4fa5548
matthieu <m.rioual83@gmail.com>
2021-12-01 11:14:00 +0100

    add experience page

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -3,7 +3,25 @@ import React from 'react'
 export const Experiences = () => {
     return (
         <div>
-            Experiences
+            <main>
+                <div className="first">
+                    <span>The start</span>
+                    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+                </div>
+                <div>
+                    <p>Shoober shooberino adorable doggo many pats, heckin good boys many pats pupper wrinkler, corgo maximum borkdrive. A frighten puggo wow very biscit.</p>
+                </div>
+                <div>
+                    <p>Big ol h*ck adorable doggo vvv smol borking doggo with a long snoot for pats big ol, he made many woofs doing me a frighten puggo wow very biscit, ruff fat boi ruff long doggo. </p>
+                </div>
+                <div>
+                    <p>Long bois mlem I am bekom fat wrinkler puggo maximum borkdrive big ol pupper I am bekom fat, fluffer vvv adorable doggo lotsa pats snoot. I am bekom fat ur givin me a spook length boy wow very biscit very good spot.</p>
+                </div>
+                <div className="last">
+                    <p>Doggo ipsum long bois lotsa pats blep. What a nice floof ruff super chub very good spot, the neighborhood pupper lotsa pats. Borkdrive shibe shoober what a nice floof, borking doggo.</p>
+                    <span>The end ?</span>
+                </div>
+            </main>
         </div>
     )
 }
